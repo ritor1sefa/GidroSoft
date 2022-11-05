78bb9f964fe62d64893741e98e8a2c9f128f4f15
ritor <ritor.sefa@gmail.com>
2022-11-05 14:26:12 +0300

    Add new project
    tmp soft for converting so much tables from doc-txt to sqlite-xls

---
+++ 97-16/b_Convert1.cs
@@ -0,0 +1,56 @@
+#pragma warning disable
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MaxyGames.Generated {
+	public class b_Convert1 : MaxyGames.RuntimeBehaviour {
+		private int index;
+
+		private void Update() {
+			string variable0 = "";
+			if(Input.GetKeyUp(KeyCode.UpArrow)) {}
+		}
+
+		public void loadFromFiles() {
+			string path = "tmp.txt";
+			System.Array file_dataL = default(System.Array);
+			string file_data = "";
+			System.Array tables = new string[0];
+			string One_table_data = "";
+			string N_table = "";
+			string N_month = "";
+			string N_year = "";
+			path = Application.streamingAssetsPath + "/" + "tmp.txt";
+			file_data = File.ReadAllText(path);
+			tables = file_data.Split("Табли", System.StringSplitOptions.RemoveEmptyEntries);
+			new _Log()._2log("Количесто таблиц в файле: " + tables.Length.ToString(), true);
+			One_table_data = tables.GetValue(1).ToString();
+			//Get Number of table
+			N_table = Regex.Match(One_table_data, "ца\\D*(\\d+)\\.\\D*\\n", RegexOptions.None).Result("$1");
+			N_month = Regex.Match(One_table_data, "Месяц\\D*(\\d+)\\D*Год\\D*(\\d+)", RegexOptions.None).Result("$1");
+			N_year = Regex.Match(One_table_data, "Месяц\\D*(\\d+)\\D*Год\\D*(\\d+)", RegexOptions.None).Result("$2");
+			_allIndexof(One_table_data);
+		}
+
+		/// <summary>
+		/// Extract all indexes of clmn delimiters
+		/// </summary>
+		public List<int> _allIndexof(string data) {
+			List<int> indexs = default(List<int>);
+			int i = 0;
+			int finded = 0;
+			List<int> result = new List<int>();
+			//Делит таблицу на строки
+			foreach(string loopObject in data.Split(System.Environment.NewLine, System.StringSplitOptions.RemoveEmptyEntries)) {
+				//Бегает по строке - ищет приключений
+				for(index = loopObject.IndexOfAny(new char[] { '╦', '┬' }); index > -1; index = loopObject.IndexOfAny(new char[] { '┬', '╦' }, (index + 1))) {
+					new _Log()._2log(index.ToString(), false);
+				}
+			}
+			return result;
+		}
+	}
+}
---
+++ 97-16/config.cs
@@ -0,0 +1,10 @@
+#pragma warning disable
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace MaxyGames.Generated {
+	public class config : MaxyGames.RuntimeBehaviour {
+
+		private void Update() {}
+	}
+}
