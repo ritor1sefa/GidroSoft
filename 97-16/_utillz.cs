43ad30cb536ade8a94e39521dca263e9550ad0f8
ritor <ritor.sefa@gmail.com>
2022-11-10 00:17:33 +0300

    Удалил конвертор в латиницу, ибо слишком слаб
    заменил на https://github.com/nick-buhro/Translit
    Вроде норм, даже в обратно может.
    хотя, по факту на текущий момент он не нужен. Потому что названия одних и тех же станций в разных файлах написаны разным шрифтом и\или с использованием латинцы в некоторых буквах. 3,1516ц!

--- 97-16/_utillz.cs
+++ 97-16/_utillz.cs
@@ -14,18 +14,4 @@ public class _utillz : object {
 		}
 		GameObject.Find("log_window").GetComponent<TMPro.TMP_InputField>().text = time + "" + text2log + System.Environment.NewLine + GameObject.Find("log_window").GetComponent<TMPro.TMP_InputField>().text;
 	}
-
-	public string _ConvertStringRusLat(string into) {
-		Dictionary<string, string> Letters2Latin = new Dictionary<string, string>() { { "а", "a" }, { "А", "A" }, { "б", "b" }, { "Б", "B" }, { "в", "v" }, { "В", "V" }, { "г", "g" }, { "Г", "G" }, { "д", "d" }, { "Д", "D" }, { "е", "e" }, { "Е", "E" }, { "ё", "yo" }, { "Ё", "Yo" }, { "ж", "j" }, { "Ж", "J" }, { "з", "z" }, { "З", "Z" }, { "и", "i" }, { "И", "I" }, { "й", "y" }, { "Й", "Y" }, { "к", "k" }, { "К", "K" }, { "л", "l" }, { "Л", "L" }, { "м", "m" }, { "М", "M" }, { "н", "n" }, { "Н", "N" }, { "о", "o" }, { "О", "O" }, { "п", "p" }, { "П", "P" }, { "р", "r" }, { "Р", "R" }, { "с", "c" }, { "С", "C" }, { "т", "t" }, { "Т", "T" }, { "у", "u" }, { "У", "U" }, { "ф", "f" }, { "Ф", "F" }, { "х", "x" }, { "Х", "X" }, { "ц", "ts" }, { "Ц", "Ts" }, { "ч", "ch" }, { "Ч", "Ch" }, { "ш", "sh" }, { "Ш", "Sh" }, { "щ", "shch" }, { "Щ", "Shch" }, { "ъ", "_b" }, { "Ъ", "_b" }, { "ы", "bI" }, { "Ы", "bI" }, { "ь", "'" }, { "Ь", "'" }, { "э", "ea" }, { "Э", "Ea" }, { "ю", "yu" }, { "Ю", "Yu" }, { "я", "ya" }, { "Я", "Ya" }, { " ", " " } };
-		string out_r = "";
-		string value = "";
-		for(int index = 0; index < into.Length; index += 1) {
-			if(Letters2Latin.TryGetValue(into.Substring(index, 1), out value)) {
-				out_r = out_r + value;
-			} else {
-				out_r = out_r + "~";
-			}
-		}
-		return out_r;
-	}
 }
