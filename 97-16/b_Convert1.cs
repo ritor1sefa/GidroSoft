0b606a0fdcf0aedc468b5a4f3551e05ca5700c08
ritor <ritor.sefa@gmail.com>
2022-11-12 16:43:45 +0300

    Очередная преАльфа.

--- 97-16/b_Convert1.cs
+++ 97-16/b_Convert1.cs
@@ -130,7 +130,6 @@ namespace MaxyGames.Generated {
 						TableNext = true;
 					}
 				}
-				sql_log("", "След файл");
 				Debug.Log("След файл");
 				sql_close();
 				FileNext = true;
@@ -146,9 +145,13 @@ namespace MaxyGames.Generated {
 			List<string> _2thPart = default(List<string>);
 			string tmp_row_raw = "";
 			string tmp_ifNextMonth = "";
+			int tmp_row_maxLenght = 0;
+			int tmp_row_hulfLenght = 0;
 			_rowsUnparsed.Clear();
 			_rowsUnparsed = new List<string>();
 			_2thPart = new List<string>();
+			tmp_row_raw = "";
+			tmp_row_maxLenght = 0;
 			switch(N_table) {
 				case "12": {
 					//Если новые 12 таблицы делить не надо
@@ -158,8 +161,21 @@ namespace MaxyGames.Generated {
 						//пилим 12 таблицу пополам
 						foreach(string loopObject in One_table_data.Split(System.Environment.NewLine, System.StringSplitOptions.None)) {
 							if(!(Regex.IsMatch(loopObject.Trim(), "^ца\\D*(\\d+)\\."))) {
-								_rowsUnparsed.Add(loopObject.Substring(0, (loopObject.Length / 2)));
-								_2thPart.Add(loopObject.Substring((loopObject.Length / 2), (loopObject.Length - (loopObject.Length / 2))));
+								tmp_row_raw = loopObject;
+								if((tmp_row_raw.Length > tmp_row_maxLenght)) {
+									//Тут всегда самая большая длина строки (из шапки)
+									tmp_row_maxLenght = tmp_row_raw.Length;
+									tmp_row_hulfLenght = (tmp_row_maxLenght / 2);
+								} else //если первая неполная
+								if((tmp_row_hulfLenght > tmp_row_raw.Length)) {
+									//Если текущая строка меньше половины максимальной
+									_rowsUnparsed.Add(tmp_row_raw.Substring(0, tmp_row_raw.Length));
+								} else {
+									//Если текущая строка больше половины максимальной
+									_rowsUnparsed.Add(tmp_row_raw.Substring(0, tmp_row_hulfLenght));
+									//второй столбец. засовываем оставшуюся строку
+									_2thPart.Add(tmp_row_raw.Substring(tmp_row_hulfLenght, (tmp_row_raw.Length - tmp_row_hulfLenght)));
+								}
 							}
 						}
 						foreach(string loopObject1 in _2thPart) {
@@ -175,13 +191,13 @@ namespace MaxyGames.Generated {
 					tmp_ifNextMonth = "";
 					//непилимые таблицы
 					foreach(string loopObject2 in One_table_data.Split(System.Environment.NewLine, System.StringSplitOptions.None)) {
+						tmp_row_raw = loopObject2;
 						if(loopObject2.Trim().ToLower().Contains("Переход".ToLower())) {
 							//если есть в строке "Переход на следующий месяц"
 							tmp_ifNextMonth = "_";
 							sql_log("Переход на следующий месяц", "");
-						} else {
-							_rowsUnparsed.Add(tmp_ifNextMonth + tmp_row_raw);
 						}
+						_rowsUnparsed.Add(tmp_ifNextMonth + tmp_row_raw);
 					}
 				}
 				break;
@@ -203,7 +219,7 @@ namespace MaxyGames.Generated {
 					row = loopObject3;
 					//Бегает по строке - ищет приключений
 					for(index = row.IndexOfAny(new char[] { '╦', '┬', '|', '¦' }); index > -1; index = row.IndexOfAny(new char[] { '┬', '╦', '|', '¦' }, (index + 1))) {
-						tmp_hash_ints.Add((index + 1));
+						tmp_hash_ints.Add(index);
 					}
 				}
 			}
@@ -253,23 +269,21 @@ namespace MaxyGames.Generated {
 					} else if((row_indexs_delimeters[0] >= line.Trim().Length)) {
 						sql_log("BD=" + line.Trim() + "==" + "Table=" + N_table + "==YM=" + N_year_N_month, "= Только название или вообще пустая строчка");
 					} else {
-						if(string.IsNullOrEmpty(Regex.Match(line.TrimStart().Substring(0, row_indexs_delimeters[0]), "^\\D*\\d+\\.(.+)", RegexOptions.None).Result("$1").Trim())) {
+						if(Regex.IsMatch(line.Substring(0, (row_indexs_delimeters[0] - 1)), "_\\D*\\d+\\.(.+)")) {
+							//если начинается с подчёркивания="Переход на следующий месяц"
+							tmp_name = "_" + Regex.Match(" " + line.Substring(0, (row_indexs_delimeters[0] - 1)).TrimStart(), "\\D*\\d+\\.(.+)", RegexOptions.None).Result("$1");
+						}
+						if(string.IsNullOrEmpty(line.Substring(0, (row_indexs_delimeters[0] - 1)).TrimStart())) {
 							//добавляем данные в первый столбец
-							tmp_line = tmp_db_name + line.Substring(tmp_db_name.Length, (line.Length - tmp_db_name.Length));
+							tmp_line = tmp_db_name + line.Trim().Substring(tmp_db_name.Length, (line.Length - tmp_db_name.Length));
 						} else {
-							if(Regex.IsMatch(line.TrimStart().Substring(0, row_indexs_delimeters[0]), "^_\\D*\\d+\\.(.+)")) {
-								//если начинается с подчёркивания="Переход на следующий месяц"
-								tmp_name = Regex.Match(line.TrimStart().Substring(0, row_indexs_delimeters[0]), "^_\\D*\\d+\\.(.+)", RegexOptions.None).Result("$1");
-							} else {
-								//если не "Переход на следующий месяц", т.е. обычный
-								tmp_name = Regex.Match(line.TrimStart().Substring(0, row_indexs_delimeters[0]), "^\\D*\\d+\\.(.+)", RegexOptions.None).Result("$1");
-							}
+							//если не "Переход на следующий месяц", т.е. обычный
+							tmp_name = Regex.Match(" " + line.Substring(0, (row_indexs_delimeters[0] - 1)).TrimStart(), "\\D*\\d+\\.(.+)", RegexOptions.None).Result("$1");
 							//сохранение имени бд, на случай пустой следующей строки
-							tmp_db_name = NickBuhro.Translit.Transliteration.CyrillicToLatin(tmp_name.Trim(), NickBuhro.Translit.Language.Russian).Replace(",", "_");
-							tmp_line = line.TrimStart();
+							tmp_db_name = NickBuhro.Translit.Transliteration.CyrillicToLatin(tmp_name.Trim(), NickBuhro.Translit.Language.Russian).ToLower().Replace(",", "_");
+							tmp_line = line.Trim();
 						}
 						sql_log("BD=" + tmp_db_name + "==" + "Table=" + N_table + "==YM=" + N_year_N_month, "");
-						//Оставляем только номер, потому что одинаковые названия на "русском" разные.
 						_rowsParsed.Add(tmp_db_name);
 						for(int index1 = 0; index1 < (row_indexs_delimeters.Count - 1); index1 += 1) {
 							from = (row_indexs_delimeters[index1] + tmp_startLine);
